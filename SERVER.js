68947e510673d8e5bbe3aded243afe88b80e0e4a
Deicide01 <125948240+Deicide01@users.noreply.github.com>
2025-06-03 16:22:05 +0530

    Add files via upload

---
+++ SERVER.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const userRoutes = require('./routes/userRoutes');
+
+app.use(express.json());
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
