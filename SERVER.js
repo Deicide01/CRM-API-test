75bc03a51332a7b1d0a767d935705ae7e32f3e14
Deicide01 <125948240+Deicide01@users.noreply.github.com>
2025-06-05 12:46:15 +0530

    Update SERVER.js

--- SERVER.js
+++ SERVER.js
@@ -3,9 +3,11 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const userRoutes = require('./routes/userRoutes');
+const supplierRoutes = require('./routes/supplierRoutes');
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
+app.use('/api/suppliers', supplierRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
